a23bba0ceb7eaf32669a472d3b52e9c718718ecb
sudhakar8184 <chintareddy.sudhakarreddy@gmail.com>
2024-07-15 16:36:50 +0530

    cron update

--- app/services/cron.js
+++ app/services/cron.js
@@ -1,7 +1,7 @@
 const cron =  require('node-cron');
 const { sendBulkMessages } = require('../controller/whatsappController');
 
-const job = cron.schedule('05 4 * * *', async() => {
+const job = cron.schedule('12 11 * * *', async() => {
     console.log('cron job started...')
     console.log(new Date())
     await sendBulkMessages()
